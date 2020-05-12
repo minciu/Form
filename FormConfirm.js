0ea78eab8acdde3317f6a895548bedd04c15d763
minciu <stefan_minciuna@yahoo.com>
2020-05-12 10:50:08 -0700

    Update FormConfirm.js

--- FormConfirm.js
+++ FormConfirm.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
-import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
+import ListGroup from "react-bootstrap/ListGroup";
 
 class FormConfirm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       success: false,
-      message: "",
+      message: " "
     };
   }
-
-  continue = (e) => {
+  state={
+    curTime : new Date().toLocaleString(),
+  }
+  continue = e => {
     e.preventDefault();
     this.props.modifySuccess(this.state.success, this.state.message);
     this.props.nextStep();
   };
 
-  back = (e) => {
+  back = e => {
     e.preventDefault();
     this.props.prevStep();
   };
@@ -29,24 +31,24 @@ class FormConfirm extends Component {
     this.props.addFormDetails(e, data);
     axios({
       method: "post",
-      url: "http://localhost:3000/api/v1/reservations",
-      data,
+      url: "http://localhost:3000/api/v1/reviews",
+      data
     })
-      .then((response) => {
+      .then(response => {
         this.setState(
           {
-            success: true,
+            success: true
           },
           () => {
             this.continue(e);
           }
         );
       })
-      .catch((err) => {
+      .catch(err => {
         this.setState(
           {
             success: false,
-            message: err.response.data.error.message,
+            message: err.response.data.error.message
           },
           () => {
             this.continue(e);
@@ -56,25 +58,26 @@ class FormConfirm extends Component {
   };
 
   render() {
-    const {
+     const {
       values: {
+    
         userName,
         score,
         description,
         timeCreated,
         timeModified,
-        helpfulness,
-      },
+        helpfulness
+      }
     } = this.props;
     let formValues = {
-      reviewerId:  "7a8h6dyd5935d8753l6c2c5k",
+      reviewerId: "7a8h6dyd5935d8753l6c2c5k",
       userName: userName,
       score: score,
       description: description,
       timeCreated: timeCreated,
       timeModified: timeModified,
-      helpfulness:helpfulness,
-      providerId: "5e8b6ecd5935d8350c6c2c2a",
+      helpfulness: helpfulness,
+      providerId: "5e8b6ecd5935d8350c6c2c2a"
     };
     return (
       <div>
@@ -83,12 +86,10 @@ class FormConfirm extends Component {
           <ListGroup.Item>Score: {score}</ListGroup.Item>
           <ListGroup.Item>Description: {description}</ListGroup.Item>
           <ListGroup.Item>TimeCreated: {timeCreated}</ListGroup.Item>
-          <ListGroup.Item>TimeModified: {timeModified}</ListGroup.Item>
-          <ListGroup.Item>Helpfulness: {helpfulness}</ListGroup.Item>
           <ListGroup.Item> Name: {userName}</ListGroup.Item>
         </ListGroup>
         <Button
-          onClick={(event) => {
+          onClick={event => {
             this.addFormDetails(event, formValues);
           }}
           variant="primary"
@@ -112,17 +113,17 @@ class FormConfirm extends Component {
 
 const styles = {
   button: {
-    backgroundColor: "#A71D31",
+    backgroundColor: "f7ead9",
     color: "#F7E7D9",
     border: "none",
     marginRight: "1vw",
     marginTop: "2vh",
     width: "auto",
-    height: "auto",
+    height: "auto"
   },
   text: {
-    color: "#2B2633",
-  },
+    color: "#2B2633"
+  }
 };
 
 export default FormConfirm;
